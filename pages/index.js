27c007500346f4ab35a13104414e58dd8187cc9c
Ammar Khan <57397520+ammarkhanone@users.noreply.github.com>
2026-01-28 21:49:51 +0500

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>live-demo-app-1149AM</h1>
+      <p>React dashboard with Tailwind CSS styling with the name demo live </p>
+    </main>
+  )
+}
